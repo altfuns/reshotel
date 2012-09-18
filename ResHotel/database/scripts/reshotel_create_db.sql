1b65b01e3d6721b758a2f1b06516accadd418436
Luis Alfonso Aguilar-Sánchez <laltfuns@gmail.com>
2012-09-17 23:18:23 -0600

    Agregando script para insertar los registros de prueba

--- ResHotel/database/scripts/reshotel_create_db.sql
+++ ResHotel/database/scripts/reshotel_create_db.sql
@@ -7,7 +7,7 @@ CREATE TABLE cliente (
 	numero_tarjeta_credito INTEGER,
 	residencia INTEGER
 );
-INSERT INTO cliente VALUES(1,111111111,'Carlos',2211145,545448484,1);
+
 CREATE TABLE habitacion (
 	id INTEGER PRIMARY KEY,
 	ocupacion_maxima INTEGER,
@@ -15,11 +15,9 @@ CREATE TABLE habitacion (
 	mobiliario TEXT,
 	costo REAL								
 );
-INSERT INTO habitacion VALUES(830705326,4,120,'TV, Refri, AC',60000.0);
-INSERT INTO habitacion VALUES(1405646000,5,123,'TV',40000.0);
+
 CREATE TABLE hotel (ubicacion INTEGER, id INTEGER PRIMARY KEY, telefono TEXT, email TEXT, clase INTEGER, nombre_persona_encargada TEXT);
-INSERT INTO hotel VALUES(1,2,2222222,'hotel2@hoteles.com',3,'Pedro');
-INSERT INTO hotel VALUES(3,1364358399,33333333,'hotel3@hoteles.com',4,'Rolando');
+
 CREATE TABLE reservacion (
 	id INTEGER PRIMARY KEY,
 	habitacion INTEGER,
@@ -34,8 +32,5 @@ CREATE TABLE ubicacion (
 	pais TEXT,
 	ciudad TEXT
 );
-INSERT INTO ubicacion VALUES(1,'Costa Rica','San José');
-INSERT INTO ubicacion VALUES(2,'Costa Rica','Guanacaste');
-INSERT INTO ubicacion VALUES(3,'Estados Unidos','Florida');
-INSERT INTO ubicacion VALUES(4,'Mexico','Cancún');
+
 COMMIT;
